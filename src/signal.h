1c6fea520b88864b7270281a3d93a364707d2ad1
xander <xavier@pop-os.localdomain>
2023-12-31 02:36:26 -0700

    Automated Update

--- src/signal.h
+++ src/signal.h
@@ -1,10 +1,26 @@
 #ifndef ERROR_H
 #define ERROR_H
 
+/**
+ * @brief Signal occurrence of error to user
+ * 
+ * @param message Formatted message to accompany error
+ * @param ... Variables to be formatted within message
+ */
 void sig_err(const char *message, ...);
-
+/**
+ * @brief Signal warning to user
+ * 
+ * @param message Formatted message to accompany warning
+ * @param ... Variables to be formatted within message
+ */
 void sig_warn(const char *message, ...);
-
+/**
+ * @brief Signal non-critical data to user
+ * 
+ * @param message Formatted message to accompany info
+ * @param ... Variables to be formatted within message
+ */
 void sig_info(const char *message, ...);
 
 #endif
