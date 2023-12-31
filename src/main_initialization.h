3a8489c9a4f45bb75e87d517795a90cb346d0323
xander <xavier@pop-os.localdomain>
2023-12-31 14:04:11 -0700

    Automated Update

--- src/main_initialization.h
+++ src/main_initialization.h
@@ -12,15 +12,15 @@ constexpr int SCREEN_WIDTH = 800;
 constexpr int SCREEN_HEIGHT = 600;
 
 /**
- * @brief 
+ * @brief Initializes SDL, creates a window and renderer, and loads a Map.
  * 
- * @param window 
- * @param renderer 
- * @param board 
- * @param argc 
- * @param argv 
- * @return true 
- * @return false 
+ * @param window A reference to a pointer to an SDL_Window that will be created.
+ * @param renderer A reference to a pointer to an SDL_Renderer that will be created.
+ * @param map A reference to a pointer to a Map that will be loaded.
+ * @param argc The number of command-line arguments.
+ * @param argv An array of strings representing the command-line arguments.
+ * @return true if the initialization is successful.
+ * @return false if the initialization fails.
  */
 bool main_initialize(SDL_Window*& window, SDL_Renderer*& renderer, Map*& map, int argc, char* argv[]);
 
