7cc5b267960bace5da53383e60ea388d1d95263b
xander <xavier@pop-os.localdomain>
2023-12-31 02:33:44 -0700

    Automated Update

--- src/main.cpp
+++ src/main.cpp
@@ -11,27 +11,11 @@ int main(int argc, char* argv[]) {
 		sig_err("Program takes map filename as sole argument");
 		return 1;
 	}
-
 	Map board(argv[1]);
 
-	board.print();
-
-	sig_err("This is an error");
-	sig_warn("This is a warning");
-	sig_info("This is information");
-
-	/*
-	board.print();
-
-	board.sonar(12, 0, 4);
-	board.print();
-
-	board.undo();
-	board.print();
-
-	board.undo();
-	board.print();
-	*/
-
+	for (;;) {
+		// input polling
+		break;
+	}
 	return 0;
 }
